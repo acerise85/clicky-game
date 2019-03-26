720e2cfb2a8027238e2ad457b442435abab0456f
acerise <42525915+acerise85@users.noreply.github.com>
2019-03-25 21:36:51 -0700

    react app initialized, files set up to start

---
+++ clicky-game/src/App.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './App.css';
+import Wrapper from "./components/Wrapper";
+
+class App extends Component {
+  render() {
+    return (
+      <Wrapper>
+        
+
+
+      </Wrapper>
+    );
+  }
+}
+
+export default App;
